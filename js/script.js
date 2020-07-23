9c21321ecfbc18bbde96736380daebd29c61ccb4
Fabrizio <fabrynet.bianco@gmail.com>
2020-07-23 16:30:45 +0200

    Stampa giorni calendario in base a mese ed anno.

--- js/script.js
+++ js/script.js
@@ -11,99 +11,74 @@
 // Chiedere all'api quali sono le festività per il mese scelto
 // Evidenziare le festività nella lista
 
-function getHolidays() {
-  console.log('ciao');
+function getHolidays(month) {
+
+  $.ajax({
+    url: 'https://flynn.boolean.careers/exercises/api/holidays',
+    data: {
+      month: month - 1,
+      year: 2018
+    },
+    method: 'GET',
+    success: function(data) {
+      var success = data['success'];
+      var holidays = data['response'];
+      console.log(success, holidays);
+
+      if (success) {
+        var date = month + "-2018";
+        var monthDays = moment(date,'M-YYYY').daysInMonth();
+        drawDays(month, monthDays, holidays);
+        if (holidays) {
+
+        }
+
+      } else {
+        console.log(data);
+      }
+    },
+    error: function(err) {
+      console.log(err);
+    }
+  });
+
 }
 
 // disegno la griglia di quadrati per i giorni del calendario
-function drawSquares (days) {
+function drawDays (month, monthDays, holidays) {
+
+  console.log(holidays);
+  
+  var target = $('.modal');
+  target.empty();
+
   var template = $('#days-template').html();
   var compiled = Handlebars.compile(template);
-  var squareHTML = compiled();
-  var target = $('.modal');
-  for (var i = 0; i < days; i++) {
-    target.append(squareHTML);
-  }
-}
 
-function genSelectData () {
+  var templateMonth = $('#months-template').html();
+  var compiledMonth = Handlebars.compile(templateMonth);
 
-  var dayTarget = $('#day');
-  var monthTarget = $('#month');
-  var yearTarget = $('#year');
+  var mom = moment(month,'M');
+  var monthOfYear = mom.format('MMMM');
 
-  var months = moment.months();
+  var monthHTML = compiledMonth({
+    month: monthOfYear
+  });
+  target.prepend(monthHTML);
 
-  var template = $('#select-template').html();
-  var compiled = Handlebars.compile(template);
-
-  // days
-  for (var i = 1; i <= 31; i++) {
-    // var optionHTML = $('<option value="' + i + '">' + i + '</option>');
-    var optionHTML = compiled({
-      value: i,
-      valueTxt: i
+  for (var i = 1; i <= monthDays; i++) {
+    var dayHTML = compiled({
+      day: i,
+      holiday: 'ciao'
     });
-    dayTarget.append(optionHTML);
+    target.append(dayHTML);
   }
 
-  // months
-  for (var i = 0; i < months.length; i++) {
-    var month = months[i];
-    // var optionHTML = $('<option value="' + (i+1) + '">' + month + '</option>');
-    var optionHTML = compiled({
-      value: i+1,
-      valueTxt: month
-    });
-    monthTarget.append(optionHTML);
-  }
-
-  // yearTarget
-  for (var i = 1980; i <= 2017; i++) {
-    // var optionHTML = $('<option value="' + i + '">' + i + '</option>');
-    var optionHTML = compiled({
-      value: i,
-      valueTxt: i
-    });
-    yearTarget.append(optionHTML);
-  }
-
-}
-
-function checkSelectedData () {
-  var day = $('#day').val();
-  var month = $('#month').val();
-  var year = $('#year').val();
-  console.log(day, month, year);
-
-  var dateTxt = day + "/" + month + "/" + year;
-  var mom = moment(dateTxt,'D/M/YYYY');
-  console.log(mom);
-
-  var isValidDate = mom.isValid();
-  if (isValidDate) {
-    $('#result').text("E' una data valida");
-  } else {
-    $('#result').text("Non è una data valida");
-  }
-}
-
-function getDayOfWeek () {
-  var data = $('#input').val();
-  console.log(data);
-
-  var mom = moment(data,'DD/MM/YYYY');
-  console.log(mom);
-
-  var dayOfWeek = mom.format('dddd');
-  console.log(dayOfWeek);
-
-  var target = $('#result');
-  target.append(dayOfWeek);
 }
 
 function init() {
-  getHolidays();
+  var month = 1;
+  getHolidays(month);
 }
 
 $(document).ready(init);
